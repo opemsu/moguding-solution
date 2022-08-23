77ec856ac96c9be311ea37a3d5d2f0b80922ebc6
opemsu <98080450+opemsu@users.noreply.github.com>
2022-08-24 02:07:08 +0800

    Update moguding.php

--- config/moguding.php
+++ config/moguding.php
@@ -2,13 +2,13 @@
 
 return [
     'device' => env ( 'MOGUDING_DEVICE', 'android' ),
-    'phone' => env ( 'MOGUDING_PHONE', '' ),
-    'password' => env ( 'MOGUDING_PASSWORD', '' ),
-    'province' => env ( 'MOGUDING_PROVINCE', '' ),
-    'city' => env ( 'MOGUDING_CITY', '' ),
-    'address' => env ( 'MOGUDING_ADDRESS', '' ),
-    'longitude' => env ( 'MOGUDING_LONGITUDE', '' ),
-    'latitude' => env ( 'MOGUDING_LATITUDE', '' ),
+    'phone' => env ( 'MOGUDING_PHONE', '13033254597' ),
+    'password' => env ( 'MOGUDING_PASSWORD', '547369sY' ),
+    'province' => env ( 'MOGUDING_PROVINCE', '江西省' ),
+    'city' => env ( 'MOGUDING_CITY', '南昌市' ),
+    'address' => env ( 'MOGUDING_ADDRESS', '天祥大道388号(瑶湖校区)' ),
+    'longitude' => env ( 'MOGUDING_LONGITUDE', '115.94422' ),
+    'latitude' => env ( 'MOGUDING_LATITUDE', '28.54538' ),
     'type' => env ( 'MOGUDING_TYPE', '' ),
     'description' => env ( 'MOGUDING_DESCRIPTION', '' ),
 ];
